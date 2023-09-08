24fa0c190c6844997bcf17f9b4b3b49a3b68aa52
unknown <Monirul-Jim>
2023-09-08 13:14:05 +0600

    check

--- src/pages/donatemoney/DonateTabMoney.js
+++ src/pages/donatemoney/DonateTabMoney.js
@@ -1,12 +1,35 @@
+// import React from 'react';
+
+// const DonateTabMoney = ({product,onPriceClick }) => {
+//     const {price}=product
+//     console.log(price);
+//     return (
+//         <div className='py-4 px-8  mx-auto border border-slate-300'>
+//         <button onClick={() => onPriceClick(price)}>$<span>{price}</span></button>
+//     </div>
+//     );
+// };
+
+// export default DonateTabMoney;
+
 import React from 'react';
 
-const DonateTabMoney = ({product,onPriceClick }) => {
-    const {price}=product
-    return (
-        <div className='py-4 px-8  mx-auto border border-slate-300'>
-        <button onClick={() => onPriceClick(price)}>$<span>{price}</span></button>
+const DonateTabMoney = ({ product, onPriceClick }) => {
+  // Check if 'product' is defined before destructuring 'price'
+  if (!product) {
+    return null; // or handle the case where 'product' is undefined
+  }
+
+  const { price } = product;
+  console.log(price);
+
+  return (
+    <div className='py-4 px-8 mx-auto border border-slate-300'>
+      <button onClick={() => onPriceClick(price)}>
+        $<span>{price}</span>
+      </button>
     </div>
-    );
+  );
 };
 
 export default DonateTabMoney;
